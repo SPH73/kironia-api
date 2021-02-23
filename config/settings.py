1645253177bf72cb243ded30cf46206f30a23716
Sue Holder <susanpholder@gmail.com>
2021-02-23 17:40:59 +0000

    added post-detail view

--- config/settings.py
+++ config/settings.py
@@ -119,20 +119,20 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    '/var/www/static/',
+]
+STATIC_ROOT = BASE_DIR / 'static_root'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media_root'
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
@@ -153,7 +153,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3080",
+    "http://localhost:3000",
     "http://127.0.0.1:3000"
 ]
 
